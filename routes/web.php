ff39d5cddd855a4080808890014e054c9cb4868b
Alexsander-oml <alexsander2018.acs@gmail.com>
2024-08-19 19:58:05 -0300

    redirect corrigido e renomeando

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\MangaController;
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('/', '/mangas');
 Route::get('/mangas', [MangaController::class, 'index'])->name('mangas.index');
 Route::get('/mangas/create', [MangaController::class, 'create'])->name('mangas.create');
 Route::post('/mangas', [MangaController::class, 'store'])->name('mangas.store');
